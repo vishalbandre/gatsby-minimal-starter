f10ab6978375ebee95cf72531646394575692444
vish <vshlbndr@gmail.com>
2021-09-17 09:29:54 +0530

    Initalized

---
+++ src/components/my-component.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { title } from './my-component-module.css'
+
+const MyComponent = () => {
+    return (
+        <h1 className={ title }>Hi, welcome to Factober Apps!</h1>
+    )
+}
+
+export default MyComponent
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const IndexPage = () => {
+  return (
+    <Layout pageTitle="Gatsby Apps">
+      <p>Welcome to Gatsby Apps.</p>
+    </Layout>
+  )
+}
+
+export default IndexPage
