5317f815de24c42f9dc164033df31c4575040287
vish <vshlbndr@gmail.com>
2021-09-17 17:11:48 +0530

    Installing Gatsby Plugins

--- src/components/my-component.js
+++ src/components/my-component.js
@@ -3,7 +3,7 @@ import { title } from './my-component-module.css'
 
 const MyComponent = () => {
     return (
-        <h1 className={ title }>Hi, welcome to Factober Apps!</h1>
+        <h1 className={ title }>Hi, welcome to Gatsby Apps!</h1>
     )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Gatsby Apps">
+    <Layout pageTitle="Home Page">
       <p>Welcome to Gatsby Apps.</p>
+      <StaticImage
+        alt="Nature"
+        src="../images/image.jpg"
+      />
     </Layout>
   )
 }
