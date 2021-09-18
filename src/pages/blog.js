ac67e5e646d0ad403b7ce9ad0e7f951f41271f69
vish <vshlbndr@gmail.com>
2021-09-18 12:07:13 +0530

    Integrated mdx plugin, reading data from filesystem and parsing

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,32 +1,41 @@
 import { graphql } from 'gatsby'
 import * as React from 'react'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../components/layout'
 
 const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="Blog Posts">
-            <p>Cool posts will go here.</p>
-            <ul>
-                {
-                    data.allFile.nodes.map(node => (
-                        <li key={node.name}>
-                            {node.name}
-                        </li>
-                    ))
-                }
-            </ul>
+            {
+                data.allMdx.nodes.map(node => (
+                    <article key={node.id}>
+                        <h2>{node.frontmatter.title}</h2>
+                        <p>Posted: {node.frontmatter.date}</p>
+                        <MDXRenderer>
+                            {node.body}
+                        </MDXRenderer>
+                    </article>
+                ))
+            }
         </Layout>
     )
 }
 
 export const query = graphql
-    `query {
-        allFile {
+    `
+    query {
+        allMdx(sort: {fields: frontmatter___date, order: DESC}) {
           nodes {
-            name
+            frontmatter {
+              date(formatString: "MMMM D, YYYY")
+              title
+            }
+            id
+            body
           }
         }
       }
+      
     `
 
 export default BlogPage
